6d760d851774eb883d7dae7ff177e7ed3f60d574
l_nagy <l_nagy@utb.cz>
2025-01-12 22:47:27 +0100

    Záloha před finálními úpravami

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SplashScreen } from "@capacitor/splash-screen";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    SplashScreen.show();
+
+    // Optional: Hide the splash screen after a delay (e.g., 3 seconds)
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000); // Adjust the duration as needed
+  }
 }
